194f6d0160811ac8da5ff75d678876b702a9ff94
n <43199306+urticaria808@users.noreply.github.com>
2025-01-12 23:25:37 +0800

    Create vector.cookie.js

---
+++ vector.cookie.js
@@ -0,0 +1,61 @@
+const authKey = 'chavy_authorization_zhanghao'  // 用于保存 Authorization Token
+const chavy = init()
+const authVal = $request.headers['Authorization']  // 获取 Authorization
+
+// 处理 Authorization
+if (authVal) {
+  if (chavy.setdata(authVal, authKey)) {
+    chavy.msg('战壕', '获取Authorization: 成功', `Token: ${authVal}`)
+    chavy.log(`[战壕] 获取Authorization: 成功, authorization: ${authVal}`)
+  }
+} else {
+  chavy.msg('战壕', '获取Authorization: 失败', '未找到 Authorization')
+}
+
+// 完成任务
+chavy.done()
+
+// 初始化函数
+function init() {
+  isSurge = () => {
+    return undefined === this.$httpClient ? false : true
+  }
+  isQuanX = () => {
+    return undefined === this.$task ? false : true
+  }
+  getdata = (key) => {
+    if (isSurge()) return $persistentStore.read(key)
+    if (isQuanX()) return $prefs.valueForKey(key)
+  }
+  setdata = (key, val) => {
+    if (isSurge()) return $persistentStore.write(key, val)
+    if (isQuanX()) return $prefs.setValueForKey(key, val)
+  }
+  msg = (title, subtitle, body) => {
+    if (isSurge()) $notification.post(title, subtitle, body)
+    if (isQuanX()) $notify(title, subtitle, body)
+  }
+  log = (message) => console.log(message)
+  get = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.get(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'GET'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  post = (url, cb) => {
+    if (isSurge()) {
+      $httpClient.post(url, cb)
+    }
+    if (isQuanX()) {
+      url.method = 'POST'
+      $task.fetch(url).then((resp) => cb(null, {}, resp.body))
+    }
+  }
+  done = (value = {}) => {
+    $done(value)
+  }
+  return { isSurge, isQuanX, msg, log, getdata, setdata, get, post, done }
+}
